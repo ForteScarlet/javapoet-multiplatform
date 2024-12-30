c6c057c9db508aa103490a558fabdd403a67a34a
ForteScarlet <ForteScarlet@163.com>
2024-12-30 16:56:28 +0800

    settings name

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,7 +20,7 @@
  *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *
  */
-rootProject.name = "simple-robot"
+rootProject.name = "javapoet-multiplatform"
 
 pluginManagement {
     repositories {
